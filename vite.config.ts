f9b794afbeb819171e0b069d55e86b7fa81ed03c
aliihsaad <aliihsaad1@gmail.com>
2025-10-27 19:02:26 +0100

    Trigger Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// Vite configuration for DeutschBoost
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
